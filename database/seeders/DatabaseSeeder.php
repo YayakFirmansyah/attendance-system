324d36731d59e8f94acec1b333a771106015d70c
Yayak Firmansyah <dhiyauddinf09@gmail.com>
2025-07-22 23:58:31 +0700

    fix: Add UserSeeder to DatabaseSeeder for complete data seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,6 +10,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            UserSeeder::class,
             StudentSeeder::class,
             CourseSeeder::class,
             ClassSeeder::class,
